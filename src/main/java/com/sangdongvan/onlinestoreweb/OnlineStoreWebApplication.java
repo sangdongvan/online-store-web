afcb208224cb2eba00e463f35637426892c7926f
Sang Dong <sang.dongvan@asnet.com.vn>
2018-03-18 19:00:24 +0700

    Initial commit

---
+++ src/main/java/com/sangdongvan/onlinestoreweb/OnlineStoreWebApplication.java
@@ -0,0 +1,12 @@
+package com.sangdongvan.onlinestoreweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreWebApplication.class, args);
+	}
+}
